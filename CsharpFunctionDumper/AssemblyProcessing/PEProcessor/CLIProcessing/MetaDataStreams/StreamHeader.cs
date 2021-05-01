5c5adcce582830de7acc5be553d5322704bbc0cc
nosharp <nosharp1@gmail.com>
2021-05-01 12:24:55 +0100

    feat: add stream header handlers

---
+++ CsharpFunctionDumper/AssemblyProcessing/PEProcessor/CLIProcessing/MetaDataStreams/StreamHeader.cs
@@ -0,0 +1,15 @@
+﻿namespace CsharpFunctionDumper.AssemblyProcessing.PEProcessor.MetaDataStreams
+{
+    public class StreamHeader
+    {
+        public uint Offset { get; private set; }
+
+        public uint Size { get; private set; }
+
+        public StreamHeader(AssemblyBuffer buffer)
+        {
+            this.Offset = buffer.ReadDWord();
+            this.Size = buffer.ReadDWord();
+        }
+    }
+}
