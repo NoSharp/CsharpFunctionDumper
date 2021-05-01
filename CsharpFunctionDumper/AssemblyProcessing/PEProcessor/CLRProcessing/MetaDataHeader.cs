f931bc6cd50ede3f7186a44ae5b08c4874fb4113
nosharp <nosharp1@gmail.com>
2021-05-01 16:25:37 +0100

    refactor: Move from miss named CLI namespace to CLR namespace

--- CsharpFunctionDumper/AssemblyProcessing/PEProcessor/CLRProcessing/MetaDataHeader.cs
+++ CsharpFunctionDumper/AssemblyProcessing/PEProcessor/CLRProcessing/MetaDataHeader.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using CsharpFunctionDumper.AssemblyProcessing.PEProcessor.MetaDataStreams;
 
-namespace CsharpFunctionDumper.AssemblyProcessing.PEProcessor
+namespace CsharpFunctionDumper.AssemblyProcessing.PEProcessor.CLRProcessing
 {
     /// <summary>
     /// The header of all .net related stuff within the Assembly.
@@ -27,17 +29,28 @@ namespace CsharpFunctionDumper.AssemblyProcessing.PEProcessor
         public ushort Flags { get; private set; }
         public ushort NumberOfStreams { get; private set; }
 
+        public StreamHeader[] Streams { get; private set; }
+
 
         public MetaDataHeader(AssemblyBuffer buffer, CLIHeader cliHeader, SectionsHeaders sectionsHeaders)
         {
             Console.WriteLine($"{cliHeader.MetaData.RVA} {sectionsHeaders.TextSection.VirtualAddress} ");
-            buffer.SetIndexPointer(cliHeader.MetaData.RVA);
+            buffer.SetIndexPointer(cliHeader.MetaData.RVA - 0x1E00);
             this.Signature = buffer.ReadDWord();
             this.MajorVersion = buffer.ReadWord();
             this.MinorVersion = buffer.ReadWord();
             this.Reserved = buffer.ReadDWord();
             this.VersionLength = buffer.ReadDWord();
             this.VersionString = buffer.ReadStringOfLength(this.VersionLength);
+            this.Flags = buffer.ReadWord();
+            this.NumberOfStreams = buffer.ReadWord();
+
+            this.Streams = new StreamHeader[this.NumberOfStreams];
+
+            for (int i = 0; i < this.NumberOfStreams; i++)
+            {
+                this.Streams[i] = new StreamHeader(buffer);
+            }
         }
 
     }
