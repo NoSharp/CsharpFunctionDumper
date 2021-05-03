570a3d0b48e857262c6b9a86e90c9fc87649e2e7
nosharp <nosharp1@gmail.com>
2021-05-03 10:45:31 +0100

    feat: add TableRow abstract class to handle the #~ table data.

---
+++ CsharpFunctionDumper/AssemblyProcessing/PEProcessor/CLRProcessing/MetaDataStreams/TableRows/TableRow.cs
@@ -0,0 +1,16 @@
+﻿namespace CsharpFunctionDumper.AssemblyProcessing.PEProcessor.CLRProcessing.MetaDataStreams.TableRows
+{
+    public abstract class TableRow
+    {
+        public MetaDataTableType OwnerTable { get; private set; }
+        public TableRow(MetaDataTableType table,AssemblyBuffer buffer)
+        {
+            this.OwnerTable = table;
+        }
+
+        public abstract void Read(AssemblyBuffer buffer);
+
+        public abstract string Display();
+
+    }
+}
