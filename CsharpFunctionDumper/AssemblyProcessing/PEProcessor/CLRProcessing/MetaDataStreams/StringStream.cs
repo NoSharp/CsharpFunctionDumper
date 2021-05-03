46bb5f3574201013c20ceb714ebbb86e6536c42c
nosharp <nosharp1@gmail.com>
2021-05-03 10:46:18 +0100

    feat: add #string stream handling

---
+++ CsharpFunctionDumper/AssemblyProcessing/PEProcessor/CLRProcessing/MetaDataStreams/StringStream.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace CsharpFunctionDumper.AssemblyProcessing.PEProcessor.CLRProcessing.MetaDataStreams
+{
+    public class StringStream : StreamHeader
+    {
+        public static StringStream INSTANCE;
+        public StringStream(AssemblyBuffer buffer, CLRHeader clrHeader) : base(buffer,clrHeader)
+        {
+            INSTANCE = this;
+            
+        }
+
+        public string ReadUntilNull(uint startOffset)
+        {
+            
+            string converted = "";
+            byte val;
+            while(true)
+            {
+                val = this._cachedBuffer[startOffset];
+                
+                if (val == 0x0)
+                {
+                    // We're reading a Null terminated string so break out of the loop.
+                    break;
+                }
+                
+                converted += (char) val;
+                startOffset++;
+            }
+
+            return converted;
+            
+        }
+
+        public string GetStringAtOffset(uint offset)
+        {
+            return this.ReadUntilNull(offset);
+        }
+
+        public override void ProcessTables(AssemblyBuffer buffer)
+        {
+        }
+    }
+}
