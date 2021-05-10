1fab0582d682552ed17d4f22b6e1d220e42bc694
nosharp <nosharp1@gmail.com>
2021-05-10 13:25:18 +0100

    feat: add parameter type handling

---
+++ CsharpFunctionDumper/CLRProcessing/MetaDataStreams/Signatures/ParameterType.cs
@@ -0,0 +1,15 @@
+﻿using CsharpFunctionDumper.AssemblyProcessing;
+
+namespace CsharpFunctionDumper.CLRProcessing.MetaDataStreams.Signatures
+{
+    public class ParameterType : IAssemblyReader
+    {
+        public TypeConstant Type { get; private set; }
+
+        public void PopulateFields(AssemblyBuffer buffer)
+        {
+            this.Type = (TypeConstant) buffer.ReadByte();
+        }
+        
+    }
+}
