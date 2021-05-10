be20456a6f92c6896671fa13ed440d204b937d30
nosharp <nosharp1@gmail.com>
2021-05-10 13:22:31 +0100

    feat: Add Calling Convetion helpers

---
+++ CsharpFunctionDumper/CLRProcessing/MetaDataStreams/Signatures/CallingConventionHelpers.cs
@@ -0,0 +1,11 @@
+﻿namespace CsharpFunctionDumper.CLRProcessing.MetaDataStreams.Signatures
+{
+    public static class CallingConventionHelpers
+    {
+        public static bool IsGeneric(this CallingConvention callingConvention)
+        {
+            return callingConvention == CallingConvention.Generic ||
+                   callingConvention == CallingConvention.HasThisAndGeneric;
+        }
+    }
+}
