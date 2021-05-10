953c40dccfb40e0026ca8cfdc6296493c949bbe8
nosharp <nosharp1@gmail.com>
2021-05-10 13:27:15 +0100

    feat: Add IAssemblyReader Interface

---
+++ CsharpFunctionDumper/CLRProcessing/MetaDataStreams/IAssemblyReader.cs
@@ -0,0 +1,10 @@
+﻿using CsharpFunctionDumper.AssemblyProcessing;
+
+namespace CsharpFunctionDumper.CLRProcessing.MetaDataStreams.Signatures
+{
+    public interface IAssemblyReader
+    {
+        public void PopulateFields(AssemblyBuffer buffer);
+
+    }
+}
