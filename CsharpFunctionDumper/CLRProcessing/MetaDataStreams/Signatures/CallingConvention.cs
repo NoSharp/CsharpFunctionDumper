a009495f51662fd69eb2ca57dc17928f782827d1
nosharp <nosharp1@gmail.com>
2021-05-10 13:22:19 +0100

    feat: Add callingconvetion Enum.

---
+++ CsharpFunctionDumper/CLRProcessing/MetaDataStreams/Signatures/CallingConvention.cs
@@ -0,0 +1,11 @@
+﻿namespace CsharpFunctionDumper.CLRProcessing.MetaDataStreams
+{
+    public enum CallingConvention
+    {
+        HasThis = 0x20,
+        Generic = 0x10,
+        HasThisAndGeneric = 0x30,
+        Default = 0x00,
+        Vararg = 0x05,
+    }
+}
