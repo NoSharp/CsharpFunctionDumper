0fe19aed89d1cbbc2854d6466a6d40ba106153ee
nosharp <nosharp1@gmail.com>
2021-05-10 13:24:05 +0100

    feat: Add return type to handle return types

---
+++ CsharpFunctionDumper/CLRProcessing/MetaDataStreams/Signatures/ReturnType.cs
@@ -0,0 +1,14 @@
+﻿using CsharpFunctionDumper.AssemblyProcessing;
+
+namespace CsharpFunctionDumper.CLRProcessing.MetaDataStreams.Signatures
+{
+    public class ReturnType : IAssemblyReader
+    {
+        public TypeConstant Type { get; private set; }
+
+        public void PopulateFields(AssemblyBuffer buffer)
+        {
+            this.Type = (TypeConstant) buffer.ReadByte();
+        }
+    }
+}
