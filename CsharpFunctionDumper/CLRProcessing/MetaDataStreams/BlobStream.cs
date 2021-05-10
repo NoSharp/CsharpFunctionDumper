7d0d67e360fecd1a2d3554c2969f2b79d181f264
nosharp <nosharp1@gmail.com>
2021-05-10 13:22:04 +0100

    feat: Improve signature handling

---
+++ CsharpFunctionDumper/CLRProcessing/MetaDataStreams/BlobStream.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+using CsharpFunctionDumper.AssemblyProcessing;
+using CsharpFunctionDumper.CLRProcessing.MetaDataStreams.Signatures;
+
+namespace CsharpFunctionDumper.CLRProcessing.MetaDataStreams
+{
+    /// <summary>
+    ///     Handles the Blob stream.
+    ///     Sources:
+    ///     https://www.codeproject.com/Articles/42649/NET-File-Format-Signatures-Under-the-Hood-Part-1-o#MethodDefSig4.3
+    ///     https://www.ecma-international.org/wp-content/uploads/ECMA-335_6th_edition_june_2012.pdf
+    /// </summary>
+    public class BlobStream : StreamHeader
+    {
+        public BlobStream(AssemblyBuffer buffer, CLRHeader clrHeader) : base(buffer, clrHeader)
+        {
+        }
+
+        public uint GetSignatureAtOffset(uint offset)
+        {
+            return _cachedBuffer[offset];
+        }
+
+        public MethodDefSignature GetMethodDefValue(uint signatureOffset)
+        {
+            _cachedAssemblyBuffer.SetIndexPointer(signatureOffset);
+            
+            MethodDefSignature signature = new MethodDefSignature();
+            signature.PopulateFields(_cachedAssemblyBuffer);
+            
+            return signature;
+        }
+
+        public override void ProcessTables(AssemblyBuffer buffer)
+        {
+        }
+    }
+}
