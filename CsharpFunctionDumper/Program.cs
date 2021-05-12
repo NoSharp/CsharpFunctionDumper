6996cf0c43f888ba01c3b856e66529a2849dd68d
nosharp <nosharp1@gmail.com>
2021-05-12 09:37:38 +0100

    feat: add command for output.

--- CsharpFunctionDumper/Program.cs
+++ CsharpFunctionDumper/Program.cs
@@ -1,12 +1,42 @@
 ﻿using System;
+using System.IO;
+using CsharpFunctionDumper.CLRProcessing.MetaDataStreams.Signatures;
 
 namespace CsharpFunctionDumper
 {
     class Program
     {
-        public static void Main(string [] args){
+        public static void Main(string [] args)
+        {
 
-            ProcessedPEFile.ProcessFile($"{Environment.CurrentDirectory}/TestFolder/Test.dll");
+            string inPath = "";
+            string outPath = "";
+            
+            for (int i = 0; i < args.Length; i+=0)
+            {
+                string argument = args[i];
+                switch (argument)
+                {
+                    case "--in":
+                    {
+                        inPath = args[i + 1];
+                        
+                        i += 2;
+                        continue;
+                    }
+                    
+                    case "--out":
+                    {
+                        outPath = args[i + 1];
+                        i += 2;
+                        continue;
+                    }
+                }
+                i += 1;
+            }
+            
+            ProcessedPEFile funcData = ProcessedPEFile.ProcessFile(inPath);
+            File.WriteAllText(outPath, funcData.GetOutput());
         }
     }
 }
